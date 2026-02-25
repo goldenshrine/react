a9211f160df516c404c9806d19f5c4af2defd7b6
prince <princeguptacss@gmail.com>
2026-02-25 18:57:59 +0530

    page scrolling

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -90,22 +90,115 @@
 ////////////////////////////////////05 Calling functions
 
 /////RA not understood anything
+// import React from "react";
+
+// const App = () => {
+//   function inputChanging(val) {
+//     console.log(val);
+//   }
+
+//   return (
+//     <div>
+//       <input
+//         onChange={function (elem) {
+//           inputChanging(elem.target.value);
+//         }}
+//         type="text"
+//         placeholder="enter name"
+//       />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+////////////////////////////////////06 functions- clientX,clientY
+
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div
+//         onMouseMove={(elem) => {
+//           console.log("value of clientY =", elem.clientY);
+//           console.log("value of clientX =", elem.clientX);
+//         }}
+//         className="box"
+//       ></div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+////////////////////////////////////07 functions- page scrolling
+
+// import React from "react";
+
+// const App = () => {
+//   const pageScrolling = () => {
+//     console.log("page scrolling...");
+//   };
+
+//   return (
+//     <div onWheel={pageScrolling}>
+//       <div className="page1"></div>
+//       <div className="page2"></div>
+//       <div className="page3"></div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+////2nd page scrolling
+
+// import React from "react";
+
+// const App = () => {
+//   const pageScrolling = (elem) => {
+//     console.log("page scrolling speed...", elem);
+//   };
+
+//   return (
+//     <div
+//       onWheel={(elem) => {
+//         pageScrolling(elem.deltaY);
+//       }}
+//     >
+//       <div className="page1"></div>
+//       <div className="page2"></div>
+//       <div className="page3"></div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//////3rd page scrolling
+
 import React from "react";
 
 const App = () => {
-  function inputChanging(val) {
-    console.log(val);
-  }
+  const pageScrolling = (elem) => {
+    if (elem > 0) {
+      console.log("scrolling-UP");
+    }
+    if (elem < 0) {
+      console.log("scrolling-DOWN");
+    }
+  };
 
   return (
-    <div>
-      <input
-        onChange={function (elem) {
-          inputChanging(elem.target.value);
-        }}
-        type="text"
-        placeholder="enter name"
-      />
+    <div
+      onWheel={(elem) => {
+        pageScrolling(elem.deltaY);
+      }}
+    >
+      <div className="page1"></div>
+      <div className="page2"></div>
+      <div className="page3"></div>
     </div>
   );
 };
