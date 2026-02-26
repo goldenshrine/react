56c2719e92d4af42f91e0bd32fa332db2224cbda
prince <princeguptacss@gmail.com>
2026-02-26 12:57:22 +0530

    without hook

---
+++ 09-useState/src/App.jsx
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////01 without hook
+
+// import React from "react";
+
+// const App = () => {
+//   let a = 20;
+//   function changeA() {
+//     console.log(a);
+//     a = 30;
+//     console.log(a);
+//   }
+//   // this does not show on the screen but just appear on the console window due to not using useState hook
+
+//   return (
+//     <div>
+//       <h1>Value of a is {a}</h1>
+//       <button onClick={changeA}>Click</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+///////////////////////////////////////////////////02 useState hook
