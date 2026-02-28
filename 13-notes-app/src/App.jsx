49fe5202eecd6ad2574bc56ee4abdbbfecfa1a9c
prince <princeguptacss@gmail.com>
2026-02-28 16:35:53 +0530

    created 13-notes-app project

---
+++ 13-notes-app/src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function App() {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+  return (
+    <div className=" h-screen lg:flex bg-black text-white ">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex gap-4 lg:w-1/2 flex-col items-start p-10"
+      >
+        <input
+          type="text"
+          placeholder="Enter Notes Heading"
+          className="px-5 py-2 font-medium w-full border-2 outline-none text-gray-200 rounded"
+        />
+
+        <textarea
+          type="text"
+          placeholder="Write Details"
+          className="px-5 py-2 h-35 w-full font-medium border-2 outline-none  text-gray-200 rounded"
+        />
+        <button className="bg-white w-full font-medium text-black px-5 py-2  outline-none border-2 rounded">
+          Add Notes
+        </button>
+      </form>
+
+      <div className="flex lg:w-1/2 bg-gray-800 gap-5 flex flex-wrap p-10">
+        <h1>Your Notes</h1>
+        <div className="h-32 w-  bg-white rounded-2xl"></div>
+        <div className="h-32 w-32  bg-white rounded-2xl"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
