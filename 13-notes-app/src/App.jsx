c0df06425987a8d1c1ce6169aa2664943b5a6b30
prince <princeguptacss@gmail.com>
2026-03-02 16:19:47 +0530

    updated project

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  copyTask.push({ title, details });
+  setTask(copyTask);
+  console.log(copyTask);
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("form submitted");
+
+    const copyTask = [...task];
+
+    copyTask.push({ title });
+
+    setTitle("");
+    setDetails("");
   };
+
   return (
     <div className=" h-screen lg:flex bg-black text-white ">
       <form
@@ -15,25 +29,35 @@ function App() {
       >
         <h1 className="text-4xl font-bold">Add Notes</h1>
 
+        {/* first input for heading */}
         <input
           type="text"
           placeholder="Enter Notes Heading"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
           className="px-5 py-2 font-medium w-full border-2 outline-none text-gray-200 rounded"
         />
 
+        {/* second input for Details */}
         <textarea
           type="text"
-          placeholder="Write Details"
-          className="px-5 py-2 h-35 w-full font-medium border-2 outline-none  text-gray-200 rounded"
+          className="px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded "
+          placeholder="Write Details here"
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
         />
-        <button className="bg-white w-full font-medium text-black px-5 py-2  outline-none border-2 rounded">
+        <button className="bg-white w-full active:scale-99 font-medium text-black px-5 py-2  outline-none border-2 rounded">
           Add Notes
         </button>
       </form>
 
       <div className="lg:w-1/2 lg:border-l-2 p-10">
         <h1 className="text-4xl font-bold">Recent Notes</h1>
-        <div className="flex flex-wrap gap-5 mt-5 h-full h-full overflow-auto">
+        <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
           <div className="h-52 w-40  bg-white rounded-2xl"></div>
           <div className="h-52 w-40  bg-white rounded-2xl"></div>
           <div className="h-52 w-40  bg-white rounded-2xl"></div>
