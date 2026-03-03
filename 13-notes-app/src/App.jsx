ab6c08cd627764e4ed615dbfbc5d69cb89a628e7
prince <princeguptacss@gmail.com>
2026-03-03 11:06:23 +0530

    updated project

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 
 function App() {
   const [title, setTitle] = useState("");
+
   const [details, setDetails] = useState("");
 
-  copyTask.push({ title, details });
-  setTask(copyTask);
-  console.log(copyTask);
+  const [task, setTask] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     const copyTask = [...task];
 
-    copyTask.push({ title });
+    copyTask.push({ title, details });
+
+    setTask(copyTask);
+    console.log(copyTask);
 
     setTitle("");
     setDetails("");
@@ -58,9 +60,19 @@ function App() {
       <div className="lg:w-1/2 lg:border-l-2 p-10">
         <h1 className="text-4xl font-bold">Recent Notes</h1>
         <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
-          <div className="h-52 w-40  bg-white rounded-2xl"></div>
-          <div className="h-52 w-40  bg-white rounded-2xl"></div>
-          <div className="h-52 w-40  bg-white rounded-2xl"></div>
+          {task.map(function (elem, idx) {
+            return (
+              <div
+                key={idx}
+                className="h-52 w-40 p-4 text-black bg-white rounded-2xl"
+              >
+                <h3 className="leading-tight text-xl font-bold">
+                  {elem.title}
+                </h3>
+                <p>{elem.details}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
