abee89e8777bc2b8c8d083612d75ddbe90f13047
prince <princeguptacss@gmail.com>
2026-03-05 19:40:18 +0530

    14 localstorage - get&set item

--- 14-localstorage/src/App.jsx
+++ 14-localstorage/src/App.jsx
@@ -1,7 +1,18 @@
+//local storage - storage in web site memory its permanent until deletd
+//session storage - storage during a session
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  // localStorage.clear() // it deletes the  local storage
+  // sessionStorage.clear() // it deletes the session  storage
+
+  localStorage.setItem("user", "sarthak");
+  localStorage.setItem("user2", "prince"); // Create or set(create) element in local storage
+  localStorage.removeItem("user1", "prince"); // Remove or dlt element in local storage
+
+  const user2 = localStorage.getItem("user2");
+  console.log(user2);
+  return <div>apple</div>;
 };
 
 export default App;
