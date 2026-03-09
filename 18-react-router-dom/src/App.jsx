ece8bd4ac2af319b26c4046d17ae65ca05071d2a
prince <princeguptacss@gmail.com>
2026-03-09 21:12:14 +0530

    react-router-dom done

--- 18-react-router-dom/src/App.jsx
+++ 18-react-router-dom/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/navbar";
 
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
